add49a961a334c795398e28bc27895e7f014b19b
mastercbhw <1024079574@qq.com>
2019-08-28 17:01:13 +0800

    使用umi脚手架下的antd-pro

---
+++ config/router.js
@@ -0,0 +1,59 @@
+export default [
+  {
+    path: '/user',
+    component: '../layouts/UserLayout',
+    routes: [
+      {
+        name: 'login',
+        path: '/user/login',
+        component: './user/login',
+      },
+    ],
+  },
+  {
+    path: '/',
+    component: '../layouts/SecurityLayout',
+    routes: [
+      {
+        path: '/',
+        component: '../layouts/BasicLayout',
+        authority: ['admin', 'user'],
+        routes: [
+          {
+            path: '/',
+            redirect: '/welcome',
+          },
+          {
+            path: '/welcome',
+            name: 'welcome',
+            icon: 'smile',
+            component: './Welcome',
+          },
+          {
+            path: '/demo',
+            name: 'demo',
+            icon: 'smile',
+            routes: [
+              {
+                path: '/demo/timebar',
+                name: 'timebar',
+                icon: 'smile',
+                component: './demo/Timebar',
+              },
+            ]
+          },
+
+          {
+            component: './404',
+          },
+        ],
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    component: './404',
+  },
+]
---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Card, Typography } from 'antd';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { FormattedMessage } from 'umi-plugin-react/locale';
+
+const CodePreview = ({ children }) => (
+  <pre
+    style={{
+      background: '#f2f4f5',
+      padding: '12px 20px',
+      margin: '12px 0',
+    }}
+  >
+    <code>
+      <Typography.Text copyable>{children}</Typography.Text>
+    </code>
+  </pre>
+);
+
+export default () => (
+  <PageHeaderWrapper>
+    aaa
+  </PageHeaderWrapper>
+);
